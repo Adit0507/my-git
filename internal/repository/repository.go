9b0ee2fef1655c21582c0a21db82548b4b47d9f9
Adit0507 <adi05072002@gmail.com>
2025-10-25 18:33:43 +0530

    getting & updatin HEAD

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -40,3 +40,45 @@ func (r *Repository) Init() error {
 
 	return nil
 }
+
+func (r *Repository) GetHEAD() (string, error) {
+	headPath := filepath.Join(r.VcsDir, "HEAD")
+	data, err := os.ReadFile(headPath)
+	if err != nil {
+		return "", err
+	}
+
+	// parsing ref: refs/head/main
+	content := string(data)
+	if len(content) > 5 && content[:5] == "ref: " {
+		refPath := filepath.Join(r.VcsDir, content[5:len(content)-1])
+		refData, err := os.ReadFile(refPath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return "", nil //no commits
+			}
+
+			return "", err
+		}
+
+		return string(refData[:len(refData)-1]), nil
+	}
+
+	return string(data[:len(data)-1]), nil
+}
+
+func (r *Repository) UpdateHEAD(commitHash string) error {
+	headPath := filepath.Join(r.VcsDir, "HEAD")
+	data, err := os.ReadFile(headPath)
+	if err != nil {
+		return err
+	}
+
+	content := string(data)
+	if len(content) > 5 && content[:5] == "ref: " {
+		refPath := filepath.Join(r.VcsDir, content[5:len(content)-1])
+		return os.WriteFile(refPath, []byte(commitHash+"\n"), 0644)
+	}
+
+	return os.WriteFile(headPath, []byte(commitHash+"\n"), 0644)
+}
