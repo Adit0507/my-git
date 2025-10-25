ca407085d0e01f618749db15332004ac6a8721be
Adit0507 <adi05072002@gmail.com>
2025-10-25 17:11:35 +0530

    creating HEAD

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "path/filepath"
+import (
+	"os"
+	"path/filepath"
+)
 
 type Repository struct {
 	Path    string
@@ -15,3 +18,25 @@ func NewRepository(path string) *Repository {
 	}
 }
 
+func (r *Repository) Init() error {
+	dirs := []string{
+		r.VcsDir,
+		filepath.Join(r.VcsDir, "objects"),
+		filepath.Join(r.VcsDir, "refs"),
+		filepath.Join(r.VcsDir, "refs", "heads"),
+	}
+
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+	}
+
+	// creatin HEAD file
+	headPath := filepath.Join(r.VcsDir, "HEAD")
+	if err := os.WriteFile(headPath, []byte("ref: refs/heads/main \n"), 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
